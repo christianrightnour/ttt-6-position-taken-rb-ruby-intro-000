6402633de49b0f7a6317d210a09c6c60255d8d44
christian Rightnour <christian_rightnour@hotmail.com>
2021-02-18 08:41:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
-def display_board(board = ["X", " ", " ", " ", " ", " ", " ", " ", " "])
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -10,5 +10,5 @@ display_board
 
 def position_taken?(board, index)
   return false if board[0] != "X" || "O"
-  return true if board[0] == "X" || "O"
+  return true if board[]
 end
